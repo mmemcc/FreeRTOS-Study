e05fbf241f8f9602aaffee37930462b0f5e87706
mmemcc <mjk980917@gmail.com>
2024-07-24 13:46:01 +0900

    example semaphore

--- main.c
+++ main.c
@@ -3,53 +3,69 @@
 /* Standard includes. */
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <errno.h>
 
 /* FreeRTOS kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "semphr.h"
+#include "timers.h"
 
 /* Local includes. */
 #include "console.h" // posix 환경에서 console을 사용하기 위한 헤더파일. printf 대신 사용
 
 // Simulation of the CPU hardware sleeping mode
 // Idle task hook, 지우면 안됨
-void vApplicationIdleHook( void )
+void vApplicationIdleHook(void)
 {
-    usleep( 15000 );
+	usleep(15000);
 }
 
-// Task 1 정의
-void vTask1( void *pvParameters )
-{
-	const char *pcTaskName = "Task 1 is running\r\n";
+#define TIMER_PERIOD pdMS_TO_TICKS(1000)
 
-	for( ;; )
-	{
-		console_print( pcTaskName );
-        vTaskDelay( 1000 );
-	}
-}
-// Task 2 정의
-void vTask2( void *pvParameters )
-{
-	const char *pcTaskName = "Task 2 is running\r\n";
+SemaphoreHandle_t xBinarySemaphore = NULL;
+TimerHandle_t xSoftwareTimer = NULL;
 
-	for( ;; )
+static void vHandlerTask(void *pvParameters)
+{
+	for (;;)
 	{
-		console_print( pcTaskName );
-        vTaskDelay( 1000 );
+		xSemaphoreTake(xBinarySemaphore, portMAX_DELAY);
+		console_print("Handler task - 세마포어를 받아 Handler Task를 실행합니다.\r\n\r\n");
 	}
 }
 
-int main( void )
+// The ISR for the software interrupt
+static void ulExampleInterruptHandler(TimerHandle_t xSoftwareTimer)
 {
-    console_init(); 
+	BaseType_t xHigherPriorityTaskWoken = pdFALSE;
 
-	xTaskCreate( vTask1, "Task 1", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vTask2, "Task 2", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-    
-	vTaskStartScheduler();
-	for( ;; );
+	xSemaphoreGiveFromISR(xBinarySemaphore, &xHigherPriorityTaskWoken);
+	console_print("ISR - 인터럽트가 발생했습니다.\r\n");
+}
+
+int main(void)
+{
+	console_init();
+
+	xBinarySemaphore = xSemaphoreCreateBinary();
+	if (xBinarySemaphore != NULL)
+	{
+		xTaskCreate(vHandlerTask, "Handler", configMINIMAL_STACK_SIZE, NULL, 1, NULL);
+
+		xSoftwareTimer = xTimerCreate("Timer", TIMER_PERIOD, pdTRUE, 0, ulExampleInterruptHandler);
+
+		if (xSoftwareTimer != NULL)
+		{
+			BaseType_t TimerStart = xTimerStart(xSoftwareTimer, 0);
+			if (TimerStart == pdPASS)
+			{
+				console_print("1초마다 발생하는 인터럽트가 생성되었습니다.\r\n");
+			}
+		}
+		vTaskStartScheduler();
+	}
+	for (;;);
 }
