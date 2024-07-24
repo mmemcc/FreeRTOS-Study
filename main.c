d8c384bac7572bee3ffe4f45cf2c47706727467c
mmemcc <mjk980917@gmail.com>
2024-07-24 13:47:47 +0900

    example queue

--- main.c
+++ main.c
@@ -3,12 +3,14 @@
 /* Standard includes. */
 #include <stdlib.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <unistd.h>
 #include <errno.h>
 
 /* FreeRTOS kernel includes. */
 #include "FreeRTOS.h"
 #include "task.h"
+#include "queue.h"
 
 /* Local includes. */
 #include "console.h" // posix 환경에서 console을 사용하기 위한 헤더파일. printf 대신 사용
@@ -20,26 +22,51 @@ void vApplicationIdleHook( void )
     usleep( 15000 );
 }
 
-// Task 1 정의
-void vTask1( void *pvParameters )
-{
-	const char *pcTaskName = "Task 1 is running\r\n";
+QueueHandle_t xQueue;
 
+static void vSenderTask( void *pvParameters )
+{
+	int32_t lValueToSend;
+	BaseType_t xStatus;
+	lValueToSend = ( int32_t ) pvParameters;
 	for( ;; )
 	{
-		console_print( pcTaskName );
-        vTaskDelay( 1000 );
+		xStatus = xQueueSendToBack( xQueue, &lValueToSend, 0 );
+		if( xStatus != pdPASS )
+		{
+			console_print( "Could not send to the queue.\r\n" );
+		}
+		else
+		{
+			console_print( "Sent = %d\r\n", lValueToSend );
+		}
+		
+		vTaskDelay( pdMS_TO_TICKS( 1000 ) );
 	}
 }
-// Task 2 정의
-void vTask2( void *pvParameters )
-{
-	const char *pcTaskName = "Task 2 is running\r\n";
 
+static void vReceiverTask( void *pvParameters )
+{
+	int32_t lReceivedValue;
+	BaseType_t xStatus;
+	const TickType_t xTicksToWait = pdMS_TO_TICKS( 100 );
+	UBaseType_t uxQueueLength;
 	for( ;; )
 	{
-		console_print( pcTaskName );
-        vTaskDelay( 1000 );
+		uxQueueLength = uxQueueMessagesWaiting( xQueue );
+		console_print( "현재 queue에 기록된 데이타 수: %u\r\n", uxQueueLength );
+
+		xStatus = xQueueReceive( xQueue, &lReceivedValue, 0 );
+		if( xStatus == pdPASS )
+			{
+			console_print( "Received = %d\r\n", lReceivedValue );
+			}
+		else
+		{
+			console_print( "Could not receive from the queue.\r\n" );
+		}
+
+		vTaskDelay( pdMS_TO_TICKS( 500 ) );
 	}
 }
 
@@ -47,9 +74,16 @@ int main( void )
 {
     console_init(); 
 
-	xTaskCreate( vTask1, "Task 1", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-	xTaskCreate( vTask2, "Task 2", configMINIMAL_STACK_SIZE, NULL, 1, NULL );
-    
-	vTaskStartScheduler();
+	xQueue = xQueueCreate( 5, sizeof( int32_t ) );
+	if( xQueue != NULL )
+	{
+		xTaskCreate( vSenderTask, "Sender1", configMINIMAL_STACK_SIZE, ( void * ) 100, 1, NULL );
+		xTaskCreate( vSenderTask, "Sender2", configMINIMAL_STACK_SIZE, ( void * ) 200, 1, NULL );
+		xTaskCreate( vReceiverTask, "Receiver", configMINIMAL_STACK_SIZE, NULL, 2, NULL );
+		vTaskStartScheduler();
+	}
+	else
+	{
+	}
 	for( ;; );
 }
